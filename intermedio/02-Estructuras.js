14e93d1214fb2e2a1eaf684827f95e47b4462201
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-05 21:43:50 -0500

    agregando nuevos metodos de arreglos

--- intermedio/02-Estructuras.js
+++ intermedio/02-Estructuras.js
@@ -53,10 +53,48 @@ console.log(palabras);
 let texto = "hola mundo cmo  la vida loca";
 console.log(texto.split(" ").slice(0, 2).join(" "));
 
-// - Ordenacion
+// ************  Ordenacion ************************
 
 let numeros = [2, 5, 6, 7, 3, 2, 9, 10, 23, 16];
 
 console.log("Sort Ordenar ----------------------------------------");
 let ordenados = numeros.sort((a, b) => a - b);
 console.log(ordenados);
+
+//reverse  - muta el array pero al reves
+console.log("Reverse ----------------------------------------");
+console.log(ordenados.reverse());
+
+// ************  Busqueda ************************
+console.log("----------------------------------------");
+console.log("----------------------------------------");
+
+//includes
+console.log("includes ----------------------------------------");
+let buscar = [3, 40, 2, 5, 4, 7, 8, 9, 10, 21, 3, 2, 5];
+console.log(buscar.includes(9));
+
+//find - devuelve el primer elemento que cumpla con la condicion
+console.log("Find ----------------------------------------");
+let nuevoBuscar = buscar.find((Element) => Element % 2 === 0);
+console.log(nuevoBuscar);
+
+//findindex - devuelve el index del  elemento que cumpla con la condicion
+console.log("FindIndex ----------------------------------------");
+let nuevoBuscar2 = buscar.findIndex((Element) => Element % 2 === 0);
+console.log(nuevoBuscar2);
+
+console.log("Set ----------------------------------------");
+let rta = buscar.filter((el, index) => buscar.indexOf(el) === index);
+let ordernado = rta.sort((a, b) => a - b);
+console.log(buscar);
+console.log(ordernado);
+
+//Elimar duplicados
+
+//Set
+
+console.log("Set ----------------------------------------");
+let muchos = [3, 40, 2, 5, 4, 7, 8, 9, 10, 21, 3, 2, 5, 1, 1, 1, 2];
+let unicos = [...new Set(muchos)];
+console.log(unicos);
