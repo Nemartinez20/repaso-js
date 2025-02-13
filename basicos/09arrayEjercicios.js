d42a12d57b7dbc8643a37d24a6aef69ab43da836
NemaWorklenovo <nestor12martinez@gmail.com>
2025-02-13 08:57:52 -0500

    learning methos forEach

--- basicos/09arrayEjercicios.js
+++ basicos/09arrayEjercicios.js
@@ -106,3 +106,44 @@ console.log(miArray2.map((el) => el * 2)); // devuelve un array , pero bajo cier
 
 let suma = pares.map((acum, numero) => acum + numero, 0);
 console.log(suma);
+console.log("Nuevo =============================================");
+
+let fruits = ["Manzana", "Mandariana", "lulo", "Pera"];
+
+fruits.forEach((Element, index, fruits) => {
+  console.log(Element, index, fruits);
+});
+
+fruits.forEach((el, index) => {
+  console.log(`el elemento es: ${el}, y su indice es ${index}`);
+});
+
+fruits.map((el, index, array) => {
+  array[1] = "Mango";
+  console.log(el);
+});
+
+let numerosArray = [2, 3, 4, 5, 6, 7, 8, 11, 13, 14, 151, 6, 12, 18];
+let pares1 = [];
+let impares = [];
+numerosArray.forEach((el) => {
+  if (el % 2 === 0) {
+    pares1.push(el);
+  } else {
+    impares.push(el);
+  }
+});
+
+console.log(pares1);
+console.log(impares);
+
+//Sumar con foreach
+let suma1 = 0;
+numerosArray.forEach((el) => {
+  suma1 += el;
+});
+
+console.log(suma1);
+//Sumar con reduce
+let total = numerosArray.reduce((acum, num) => acum + num, 0);
+console.log(total);
