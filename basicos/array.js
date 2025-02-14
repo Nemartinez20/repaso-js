5bb8f53ba565ffdcb7e6c7600540788c035ff96e
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-13 20:48:14 -0500

    agregando filtros

--- basicos/array.js
+++ basicos/array.js
@@ -25,3 +25,33 @@ let array6 = new Array("hola", "mundo", "nestor");
 
 console.log(array5);
 console.log(array6);
+
+const productos = [
+  { elemento: "Laptop", precio: 1200, categoria: "Electrónica" },
+  { elemento: "Teléfono", precio: 800, categoria: "Electrónica" },
+  { elemento: "Silla", precio: 150, categoria: "Muebles" },
+  { elemento: "Mesa", precio: 300, categoria: "Muebles" },
+  { elemento: "Camiseta", precio: 25, categoria: "Ropa" },
+  { elemento: "Zapatos", precio: 60, categoria: "Ropa" },
+  { elemento: "Audífonos", precio: 100, categoria: "Electrónica" },
+  { elemento: "Refrigerador", precio: 900, categoria: "Electrodomésticos" },
+  { elemento: "Microondas", precio: 250, categoria: "Electrodomésticos" },
+  { elemento: "Libro", precio: 620, categoria: "Papelería" },
+  { elemento: "cuaderno", precio: 420, categoria: "Papelería" },
+  { elemento: "Lapiz", precio: 520, categoria: "Papelería" },
+];
+
+const ob = {
+  precio: 400,
+  categoria: "Papelería",
+};
+
+function filtra(productos) {
+  return productos.filter(
+    (producto) =>
+      producto.precio >= ob.precio &&
+      (ob.categoria === "all" || producto.categoria == ob.categoria)
+  );
+}
+
+console.log(filtra(productos));
