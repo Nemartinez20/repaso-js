68ac200dc502751cbdbd0f6c87c42ac404c15fc9
NemaWorklenovo <nestor12martinez@gmail.com>
2025-02-18 10:19:07 -0500

    agregando ejercicio de map inmutable

--- basicos/Metodos Arrays/07 map.js
+++ basicos/Metodos Arrays/07 map.js
@@ -9,9 +9,64 @@ for (let i = 0; i < lettras.length; i++) {
 console.log(newLetters);
 
 //con map
-
 let inicio = lettras.map((el) => {
   return el;
 });
 
 console.log(inicio);
+
+console.log("================================================");
+
+const ordenes = [
+  {
+    cliente: "juan",
+    total: 60,
+    otro: true,
+  },
+  {
+    cliente: "pedro",
+    total: 100,
+    otro: true,
+  },
+  {
+    cliente: "Victoria",
+    total: 80,
+    otro: true,
+  },
+  {
+    cliente: "David",
+    total: 140,
+    otro: false,
+  },
+  {
+    cliente: "Diego",
+    total: 150,
+    otro: true,
+  },
+  {
+    cliente: "Cristian",
+    total: 90,
+    otro: true,
+  },
+  {
+    cliente: "Laura",
+    total: 120,
+    otro: false,
+  },
+];
+
+let resultado = ordenes.map((orden) => orden.total);
+console.log(ordenes);
+console.log(resultado);
+
+// Sin mutaciones
+let resultado2 = ordenes.map((orden, array) => {
+  //orden.impuesto = 0.19;
+
+  return {
+    ...orden,
+    impuesto: 0.19,
+  };
+});
+
+console.log(resultado2);
