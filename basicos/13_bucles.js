5f5ddadcb5fcb290e364c83054d856f8172ca873
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-24 21:07:57 -0500

    actualizando file

--- basicos/13_bucles.js
+++ basicos/13_bucles.js
@@ -4,3 +4,10 @@ let miarrray = [2, 3, 4, 5, 6, 7, 8, 9, 10];
 for (let i = 0; i < miarrray.length; i++) {
   console.log(i);
 }
+
+//Buscar numero pares
+for (let i = 0; i < miarrray.length; i++) {
+  if (miarrray[i] % 2 == 0) {
+    console.log("numero pas", i);
+  }
+}
