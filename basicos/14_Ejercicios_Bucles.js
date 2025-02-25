6e44d10e7991edc8d5b1f017b3ef930a85c90cc3
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-24 21:30:03 -0500

    agregando ejercicios de bucles

---
+++ basicos/14_Ejercicios_Bucles.js
@@ -0,0 +1,35 @@
+//
+
+//
+// NOTA: Explora diferentes sintaxis de bucles para resolver los ejercicios
+
+// 1. Crea un bucle que imprima los números del 1 al 20
+console.log("Ejericicio 1 =======================");
+for (i = 0; i <= 20; i++) {
+  console.log(i);
+}
+
+// 2. Crea un bucle que sume todos los números del 1 al 100 y muestre el resultado
+console.log("Ejericicio 2 ========================");
+
+let suma = 0;
+for (i = 0; i <= 100; i++) {
+  suma = suma + i;
+  //   console.log(i);
+}
+console.log("la suma es", suma);
+// 3. Crea un bucle que imprima todos los números pares entre 1 y 50
+
+// 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+
+// 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
+
+// 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+
+// 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+
+// 8. Usa un bucle para invertir una cadena de texto
+
+// 9. Usa un bucle para generar los primeros 10 números de la secuencia de Fibonacci
+
+// 10. Dado un array de números, usa un bucle para crear un nuevo array que contenga solo los números mayores a 10
