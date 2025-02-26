1781f0f9fc1a09e0a99590f9c03a0df28f1b7b5c
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-25 21:27:32 -0500

    agregando ejercicios de bucles

--- basicos/14_Ejercicios_Bucles.js
+++ basicos/14_Ejercicios_Bucles.js
@@ -20,13 +20,66 @@ for (i = 0; i <= 100; i++) {
 console.log("la suma es", suma);
 // 3. Crea un bucle que imprima todos los números pares entre 1 y 50
 
+console.log("Ejericio 3 ========================");
+
+for (i = 0; i <= 50; i++) {
+  if (i % 2 === 0) {
+    console.log("Es numero par", i);
+  }
+}
 // 4. Dado un array de nombres, usa un bucle para imprimir cada nombre en la consola
+console.log("Ejericio 4 ========================");
+
+const nombres = ["juan", "pedro", "Ariosto", "Cristian", "Bernabe"];
+
+for (i = 0; i < nombres.length; i++) {
+  console.log(nombres[i]);
+}
 
 // 5. Escribe un bucle que cuente el número de vocales en una cadena de texto
+console.log("Ejericio 5 ========================");
+
+const cadena = "mi perro se comio la tera, profesora";
+let suma1 = 0;
+for (i = 0; i < cadena.length; i++) {
+  // console.log("la tares", cadena[i]);
+
+  if (
+    cadena[i] == "a" ||
+    cadena[i] == "e" ||
+    cadena[i] == "i" ||
+    cadena[i] == "o" ||
+    cadena[i] == "u"
+  ) {
+    console.log("la tares", cadena[i]);
+
+    console.log("uno");
+    console.log((suma1 = suma1 + 1));
+    // return (suma1 = suma1 + 1);
+  }
+}
+// console.log(cadena.includes("a"));
+
+console.log(suma1);
 
 // 6. Dado un array de números, usa un bucle para multiplicar todos los números y mostrar el producto
+console.log("Ejericio 6 ========================");
 
+const multiplicar = [2, 3, 4, 5, 6, 7, 8];
+
+let resulMult = 1;
+for (i = 0; i < multiplicar.length; i++) {
+  resulMult = resulMult * multiplicar[i];
+}
+
+console.log("multiplicacion", resulMult);
 // 7. Escribe un bucle que imprima la tabla de multiplicar del 5
+console.log("Ejericio 7 ========================");
+
+for (i = 1; i <= 10; i++) {
+  mult = 4 * i;
+  console.log(`4 * ${i} =  ${mult}`);
+}
 
 // 8. Usa un bucle para invertir una cadena de texto
 
