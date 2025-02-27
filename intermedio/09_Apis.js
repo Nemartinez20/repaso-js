6f76c9737fb58090163ef7d2d4681e96abcc090e
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-02-26 21:44:41 -0500

    Se agrega consultas de apis por async await

--- intermedio/09_Apis.js
+++ intermedio/09_Apis.js
@@ -15,8 +15,28 @@
 // Consultando Api JSON-PLACEHOLDER
 url = "https://jsonplaceholder.typicode.com/albums/1/photos";
 fetch(url)
-  .then((respuesta) => console.log(respuesta))
-  //   .then((resultado) => console.log(resultado))
+  .then((respuesta) => {
+    // Transforma la respuesta a JSON
+    return respuesta.json();
+  })
+  .then((resultado) => {
+    // Procesa los datos
+    console.log(resultado);
+  })
   .catch((error) => {
     console.log(error);
   });
+
+//Con Async Await
+const consultarApi = async () => {
+  try {
+    url2 = "https://jsonplaceholder.typicode.com/albums/1/photos";
+    const respuesta2 = await fetch(url2);
+    const resultado = await respuesta2.json();
+    console.log(resultado);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+consultarApi();
