9f14848dfec914d1f40324f1374c3120528be1fe
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-03-04 20:49:52 -0500

    agregando objetos acceso

--- basicos/16_Objetos.js
+++ basicos/16_Objetos.js
@@ -1,6 +1,18 @@
 //Objetos
 
+// Sintaxis
+
 let persona = {
   nombre: "juan",
   edad: 25,
 };
+
+// Acceso a propiedades
+
+// Notación punto
+console.log(persona.name);
+
+// Notación de corchetes
+console.log(persona["name"]);
+
+// Modificación de propiedades
