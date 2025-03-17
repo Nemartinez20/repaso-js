7086d44891a762d7c6acbd018d9fdac4733a78b9
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-03-16 21:46:02 -0500

    destructuracion de arrays con valores por defecto

--- basicos/18_destructuracion-propagacion.js
+++ basicos/18_destructuracion-propagacion.js
@@ -18,6 +18,12 @@ console.log("Destructuracion....");
 const [uno, dos, tres, cuatro] = array;
 console.log(uno);
 
+//Array con valores por defecto
+const [uno1 = 0, dos2 = 0, tres2 = 0, cu0 = 0, cinco = 0, seis = 0, siete = 0] =
+  array;
+console.log(uno1);
+console.log(siete);
+
 //Objetos
 const { nombre } = persona;
 console.log(nombre);
