b105f69e23eba4d9bc723a6368a38cd81161a57b
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-03-26 21:11:01 -0500

    se agrega metodo en las clases

--- basicos/20_clases.js
+++ basicos/20_clases.js
@@ -15,8 +15,8 @@ class Persona {
 let juan = new Persona("Juan", 24, "juan@correo.com");
 console.log(juan);
 
+// ============================  EJRCICIO 2 =================
 //Clases con valores por defecto
-
 class Persona1 {
   constructor(name = "Lorena", lastname = "Camargo", ege = 34) {
     this.name = name;
@@ -28,3 +28,31 @@ class Persona1 {
 //No se le pasa árametros a la clase ya que los tiene por defecto
 let Maria = new Persona1();
 console.log(Maria);
+
+// ============================  EJRCICIO 3 =================
+
+//Acceder a las propiedades de la clase
+console.log(Maria.ege); // 34
+console.log(Maria["lastname"]); // Camargo
+
+// ============================  CLASES CON FUNCIONES  =================
+
+//1. definir la clse con su constructor y su metodos
+class Persona2 {
+  constructor(name, lastname, age) {
+    this.name = name;
+    this.lastname = lastname;
+    this.age = age;
+  }
+
+  //funcion o metodo
+  saludar() {
+    console.log(`Hola como estan me llamo ${this.name}`);
+  }
+}
+
+//Se instancia la persona
+let rtaPersona2 = new Persona2("Pedro", "Morales", 25);
+
+//Se ejecuta el metodo
+rtaPersona2.saludar();
