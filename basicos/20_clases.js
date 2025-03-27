3cc7e668739616a7d408ce2de0f2c52a6b7f75f4
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-03-26 21:02:00 -0500

    agregando clases con valores por default

--- basicos/20_clases.js
+++ basicos/20_clases.js
@@ -14,3 +14,17 @@ class Persona {
 
 let juan = new Persona("Juan", 24, "juan@correo.com");
 console.log(juan);
+
+//Clases con valores por defecto
+
+class Persona1 {
+  constructor(name = "Lorena", lastname = "Camargo", ege = 34) {
+    this.name = name;
+    this.lastname = lastname;
+    this.ege = ege;
+  }
+}
+
+//No se le pasa árametros a la clase ya que los tiene por defecto
+let Maria = new Persona1();
+console.log(Maria);
