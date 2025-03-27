7aacfd8ec78e0d181092666d054089e7e48d6d38
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-03-26 20:58:23 -0500

    creando clases con su constructor

--- basicos/20_clases.js
+++ basicos/20_clases.js
@@ -1,3 +1,16 @@
-// archivo de clases
-console.log("desde archivo de clases");
-console.log("pobadno commitsde git");
+// Clases
+
+//1. crear la clase
+class Persona {
+  //2. crear el constructor
+  constructor(name, edad, correo) {
+    this.name = name;
+    this.edad = edad;
+    this.correo = correo;
+  }
+}
+
+//3. Instanciar
+
+let juan = new Persona("Juan", 24, "juan@correo.com");
+console.log(juan);
