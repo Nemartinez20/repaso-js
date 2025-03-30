8aeea0a0ffbefb17e5586a8cdefb524aa11063dd
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-03-30 17:41:46 -0500

    agrega ejercicio d arrays

---
+++ basicos/Metodos Arrays/10_arays.js
@@ -0,0 +1,17 @@
+const miArray = [1, 2, 3, 4, 5, 6];
+
+//acceder
+console.log(miArray[2]);
+
+primeros = miArray.slice(0, 3);
+segundos = miArray.slice(3);
+console.log(primeros);
+console.log(segundos);
+
+let texto = "Hola mundo estory aprendiedo a programr";
+array = texto.split(" "); //convertir en u array  tomando lo espacios
+newarray = array.slice(0, 3); //obtiene los primeros
+finalString = newarray.join(" ");
+console.log(array);
+console.log(newarray);
+console.log(finalString);
