b1deae8cea2967cc0ca91ed79986452bbbdfd20e
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-04-07 20:46:26 -0500

    agregangdo el finally

--- basicos/22_Manejo_errores.js
+++ basicos/22_Manejo_errores.js
@@ -13,3 +13,28 @@ try {
   // Bloque de errores
   console.log("Se ha producido un error");
 }
+
+//Cpturar el mensaje del error
+
+try {
+  //Codigo que intenta ejecutar
+  console.log(person.name);
+  console.log("Finaliza la ejecucion sin errores");
+} catch (error) {
+  // Bloque de errores
+  console.log(error.message);
+  console.log("Se ha producido un error");
+}
+
+//con finally
+try {
+  //Codigo que intenta ejecutar
+  console.log(person.name);
+  console.log("Finaliza la ejecucion sin errores");
+} catch (error) {
+  // Bloque de errores
+  console.log("Se ha producido un error");
+} finally {
+  //se ejecuta siempre asi se cumpla el try o el catch
+  console.log("siempre se ejecuta");
+}
