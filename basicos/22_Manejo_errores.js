c8d0a9a06ca4390356960ccfa8dfd66273eb3aa0
Nestor20MartinezHouse <nestor20martinez@gmail.com>
2025-04-07 20:43:58 -0500

    manejo de errores

--- basicos/22_Manejo_errores.js
+++ basicos/22_Manejo_errores.js
@@ -1,2 +1,15 @@
 //Manejo de errorres
 console.log("desde errores...");
+
+let person;
+
+//Captura de errores
+//try catch
+try {
+  //Codigo que intenta ejecutar
+  console.log(person.name);
+  console.log("Finaliza la ejecucion sin errores");
+} catch (error) {
+  // Bloque de errores
+  console.log("Se ha producido un error");
+}
